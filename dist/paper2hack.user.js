888bfa98948115ad646c80640d833ed3c98ab62a
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-01-25 15:29:52 -0800

    BATTLE ROYALE SUPPORT

--- dist/paper2hack.user.js
+++ dist/paper2hack.user.js
@@ -5,6 +5,7 @@
 // @author       its-pablo
 // @match        https://paper-io.com
 // @match        https://paper-io.com/teams/
+// @match        https://paper-io.com/battleroyale/
 // @match        https://paperanimals.io
 // @match        https://amogus.io
 // @require      https://cdn.jsdelivr.net/npm/lil-gui
@@ -15,7 +16,7 @@ adblock = () => false //this detects if adblock is on, we make it always return 
 window.addEventListener('load', function () {
   "use strict";
   const VERSION = "beta 0.1.10"
-  let newApi = (location.href === "https://paper-io.com/teams/" || location.href === "https://paperanimals.io/" || location.href === "https://amogus.io")
+  let newApi = (location.href === "https://paper-io.com/battleroyale/" || location.href === "https://paper-io.com/teams/" || location.href === "https://paperanimals.io/" || location.href === "https://amogus.io")
   window.api = {
       config: function(){
           if(newApi){
